d9335f506c43e35214c4938bae208202bd3d1542
Joshua Cody <jsmith7@uoregon.edu>
2015-03-16 14:53:04 -0700

    initial commit

---
+++ problem8.c
@@ -0,0 +1,8 @@
+/*#######################################################
+# Author: Joshua Smith
+# Date Created: March 16, 2015
+# Date Last Modified: March 16, 2015
+# Description: Find the thirteen adjacent digits in the 
+#				1000-digit number that have the greatest 
+#				product
+#######################################################*/
