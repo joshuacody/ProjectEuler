1a639de622efe2df45fd13c83e1a41d3df0820bb
Joshua Cody <jsmith7@uoregon.edu>
2015-03-15 17:31:25 -0700

    solved
    Problem 7 solution

---
+++ problem7.c
@@ -0,0 +1,46 @@
+/*#######################################################
+# Author: Joshua Smith
+# Date Created: March 3, 2015
+# Date Last Modified: March 3, 2015
+# Description: Finds the 10,001st prime number using Sieve
+#				of Eratosthenes
+#######################################################*/
+
+#include <stdio.h>
+
+#define LIMIT 1000000 //size of natural numbers array
+#define TARGET 10001 //target prime number
+
+int main(){
+
+	int i, j;
+	int count = 0;
+	int numArr[LIMIT];
+
+	//populate numArr with natural numbers 
+	for(i = 0 ; i < LIMIT ; i++){
+		numArr[i] = i+2;	
+	}
+
+	//sieves the non prime numbers
+	for(i = 0 ; i < LIMIT ; i++){
+		if(numArr[i] != 0){
+			for(j = i + numArr[i] ; j < LIMIT ; j += numArr[i]){	//set J equal to i + numArr[i] to increment through array at the correct position
+				numArr[j] = 0;	//multiple of the number are set to 0 to be skipped over in the next loop iteration
+			}
+		}
+	}
+
+	//iterates through array to tally prime numbers
+	for(i = 0 ; i < LIMIT ; i++){
+		if(numArr[i] != 0){
+			count++;		//counts the number of primes found
+		}
+		if(count == TARGET){
+			printf("Prime number #%d is: %d\n", TARGET, numArr[i]);
+			break;
+		}
+	}
+
+	return 0;
+}
